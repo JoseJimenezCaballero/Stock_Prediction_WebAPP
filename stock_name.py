223ec870ab516dfb770109e6d63fb75b94742b21
JoseJimenezCaballero <143767138+JoseJimenezCaballero@users.noreply.github.com>
2024-02-20 09:39:02 -0500

    Create stock_name.py
    missed file on deployment

---
+++ stock_name.py
@@ -0,0 +1,14 @@
+import requests
+
+#fucntion allows user to simply enter company name and the function will return the ticker
+def get_ticker(company_name):
+    yfinance = "https://query2.finance.yahoo.com/v1/finance/search"
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'
+    params = {"q": company_name, "quotes_count": 1, "country": "United States"}
+
+    res = requests.get(url=yfinance, params=params, headers={'User-Agent': user_agent})
+    data = res.json()
+
+    company_code = data['quotes'][0]['symbol']
+    return company_code
+
